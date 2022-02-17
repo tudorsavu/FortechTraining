1f56285ba616b04801d6561e9dec7824e0b7403f
Tudor Savu <tudorsavu2015@gmail.com>
2022-02-17 10:52:50 +0200

    course 2, my writing

---
+++ Curs2/script.js
@@ -0,0 +1,17 @@
+//"use strict"  -- for whole file
+
+console.log("Hello world!");
+
+console.error("Big error");
+
+function doSmth() {
+  "use strict"; //strict mode for function
+}
+
+var name = "Jane";
+let address = "street x";
+const age = 20; // initializare obligatorie -- constanta
+const arr = [1, 2, 3];
+arr.push(5);
+
+console.log(arr);
